2d9204523476a04b3488942a814ee73b35ec6b28
Dai <kdmgs110@gmail.com>
2017-01-28 13:54:58 +0000

    edited omniAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  #provider :facebook, ENV['353667681638505'], ENV['2f957e1f8e4fcfb6742506115f2f2dd0']
- provider :facebook,'353667681638505', '2f957e1f8e4fcfb6742506115f2f2dd0', scope: "email,user_education_history,user_friends"
+ provider :facebook,'1664698063822824', 'db98fd965b145f14bcd1c1606188d160', scope: "email,user_education_history,user_friends"、:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
  end
