9eacd5a69b005840b579509ea72549f10e0085c5
Dai <kdmgs110@gmail.com>
2017-01-30 02:26:07 +0000

    added end in amniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
- scope: "email,user_education_history,user_friends",
+ scope: "email,user_education_history,user_friends"
 end
