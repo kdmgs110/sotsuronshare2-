594be2c80a350c5c3835d81db604cbd606c068ce
Dai <kdmgs110@gmail.com>
2017-01-30 02:24:15 +0000

    adjusted facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
- #provider :facebook, ENV['353667681638505'], ENV['2f957e1f8e4fcfb6742506115f2f2dd0']
- provider :facebook,'1664698063822824', 'db98fd965b145f14bcd1c1606188d160', 
- scope: "email,user_education_history,user_friends",:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
- OmniAuth.config.full_host = "https://radiant-citadel-12975.herokuapp.com"
- end
+ provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+ scope: "email,user_education_history,user_friends",
+end
