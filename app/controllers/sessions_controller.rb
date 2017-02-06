5e3be8bd0c7e9bf08b0bf62521ecde34839080f7
Dai <kdmgs110@gmail.com>
2017-02-06 14:34:44 +0000

    edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to users_url,notice:"ログインしました"
+    redirect_to users_url,notice:"ログインしました。ユーザの編集は済みましたか？右上の設定ページから編集しましょう。"
   end
   def destroy
     session[:user_id] = nil
