b0f8cad45a64f18c330f48627ba8d211be6a9c36
Dai <kdmgs110@gmail.com>
2017-02-08 04:38:19 +0000

    fixed authentification

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
 before_filter :set_search
     def home 
-        
+        if current_user.present?
+         redirect_to users_path,notice: "#{current_user.name}さんとしてログインしています"
+        end
     end
 end
