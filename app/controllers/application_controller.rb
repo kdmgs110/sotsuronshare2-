a8d804f8d6923a8a5ac7264bc1afe2855b10c72f
Dai <kdmgs110@gmail.com>
2017-02-08 04:20:11 +0000

    implmented authenticate users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  
+        
   def set_search
     @q        = User.search(params[:q])
     @users = @q.result.paginate(page: params[:page], per_page: params[:per_page])
@@ -18,12 +18,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   private
   
-    def requires_logged_in
-      if current_user == nil
-        redirect_to root_url #Only happens if current_user is equal to nil
-      end
-    end
-    
     def current_user
       begin
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
