a3e7c70b0396255a0f8e346a23e5a0532417f893
Dai <kdmgs110@gmail.com>
2017-03-07 08:31:35 +0000

    fixed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
 before_action :set_pending, except: [:home,:terms,:profile]
 
-
     def home 
         if current_user.present?
          redirect_to users_path,notice: "#{current_user.name}さんとしてログインしています"
@@ -9,9 +8,14 @@ before_action :set_pending, except: [:home,:terms,:profile]
     end
     
     def terms
-        
+        if current_user.present?
+            set_pending
+        end
     end
     
     def profile
+        if current_user.present?
+            set_pending
+        end
     end
 end
